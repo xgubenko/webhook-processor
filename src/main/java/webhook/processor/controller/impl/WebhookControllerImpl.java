74704116e7ecef322e8e8b0550343e8a6899eec8
xgubenko <xgubenko@gmail.com>
2025-01-11 13:28:07 +0300

    binance update

--- src/main/java/webhook/processor/controller/impl/WebhookControllerImpl.java
+++ src/main/java/webhook/processor/controller/impl/WebhookControllerImpl.java
@@ -1,48 +1,48 @@
-package webhook.processor.controller.impl;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import webhook.processor.controller.WebhookController;
-import webhook.processor.dto.TradingViewRequest;
-import webhook.processor.dto.FinamTransactionDirection;
-import webhook.processor.service.FinamOrderService;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/v1/webhook")
-public class WebhookControllerImpl implements WebhookController {
-
-    private final FinamOrderService orderService;
-
-    @Override
-    @PostMapping(consumes="text/plain")
-    public ResponseEntity<Object> createNewOrder(@RequestBody String request) {
-        orderService.process(initRequest(request));
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
-
-    @Override
-    @PostMapping(value="/token", consumes="text/plain;charset=UTF-8")
-    public ResponseEntity<Object> checkToken(@RequestBody String request) {
-        return ResponseEntity.status(HttpStatus.OK).body(orderService.checkToken(initRequest(request)));
-    }
-
-    private TradingViewRequest initRequest(String s) {
-        TradingViewRequest  request = new TradingViewRequest();
-
-        //clientId api code quantity direction
-        String[] arr = s.split(" ");
-
-        request.setClientId(arr[0]);
-        request.setApi(arr[1]);
-        request.setCode(arr[2]);
-        request.setQuantity(Integer.parseInt(arr[3]));
-
-        if(arr[4].equals("buy")) request.setDirection(FinamTransactionDirection.Buy);
-        else request.setDirection(FinamTransactionDirection.Sell);
-
-        return request;
-    }
-}
+//package webhook.processor.controller.impl;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import webhook.processor.controller.WebhookController;
+//import webhook.processor.dto.TradingViewRequest;
+//import webhook.processor.dto.FinamTransactionDirection;
+//import webhook.processor.service.FinamOrderService;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/api/v1/webhook")
+//public class WebhookControllerImpl implements WebhookController {
+//
+//    private final FinamOrderService orderService;
+//
+//    @Override
+//    @PostMapping(consumes="text/plain")
+//    public ResponseEntity<Object> createNewOrder(@RequestBody String request) {
+//        orderService.process(initRequest(request));
+//        return ResponseEntity.status(HttpStatus.OK).build();
+//    }
+//
+//    @Override
+//    @PostMapping(value="/token", consumes="text/plain;charset=UTF-8")
+//    public ResponseEntity<Object> checkToken(@RequestBody String request) {
+//        return ResponseEntity.status(HttpStatus.OK).body(orderService.checkToken(initRequest(request)));
+//    }
+//
+//    private TradingViewRequest initRequest(String s) {
+//        TradingViewRequest  request = new TradingViewRequest();
+//
+//        //clientId api code quantity direction
+//        String[] arr = s.split(" ");
+//
+//        request.setClientId(arr[0]);
+//        request.setApi(arr[1]);
+//        request.setCode(arr[2]);
+//        request.setQuantity(Integer.parseInt(arr[3]));
+//
+//        if(arr[4].equals("buy")) request.setDirection(FinamTransactionDirection.Buy);
+//        else request.setDirection(FinamTransactionDirection.Sell);
+//
+//        return request;
+//    }
+//}
--- src/main/java/webhook/processor/service/impl/BinanceOrderServiceImpl.java
+++ src/main/java/webhook/processor/service/impl/BinanceOrderServiceImpl.java
@@ -1,43 +1,52 @@
 package webhook.processor.service.impl;
 
-import com.binance.connector.client.impl.SpotClientImpl;
+import com.binance.connector.futures.client.impl.CMFuturesClientImpl;
+import com.binance.connector.futures.client.impl.FuturesClientImpl;
+import com.binance.connector.futures.client.impl.UMFuturesClientImpl;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import webhook.processor.dto.BinanceTradingViewRequest;
+import webhook.processor.properties.BinanceProperties;
 import webhook.processor.service.BinanceOrderService;
 
 import java.util.LinkedHashMap;
 
 @Service
 @Slf4j
+@AllArgsConstructor
 public class BinanceOrderServiceImpl implements BinanceOrderService {
+
+    private final BinanceProperties properties;
 
     @Override
     public void process(BinanceTradingViewRequest request) {
         log.info("Start processing request: {}", request);
-        LinkedHashMap<String, Object> parameters = new LinkedHashMap<String, Object>();
+        LinkedHashMap<String, Object> parameters = new LinkedHashMap<>();
 
-        SpotClientImpl client = new SpotClientImpl(request.getApi(), request.getSecret());
-
-//                 Example params
-//        parameters.put("symbol","BTCUSDT");
-//        parameters.put("side", "SELL");
-//        parameters.put("type", "LIMIT");
-//        parameters.put("timeInForce", "GTC");
-//        parameters.put("quantity", 0.01);
-//        parameters.put("price", 9500);
+        FuturesClientImpl client = new UMFuturesClientImpl(properties.getKey(), properties.getSecret());
+        System.out.println(properties.getKey() + "\n" +  properties.getSecret());
 
         parameters.put("symbol", request.getCode());
         parameters.put("side", request.getDirection());
         parameters.put("type", "MARKET");
-//        parameters.put("timeInForce", "GTC");
+        parameters.put("quantity", request.getQuantity());
 
-        double quantity = request.getQuantity();
-//        if(TRADING_IN_PROGRESS) quantity  = quantity * 2;
-        parameters.put("quantity", quantity);
+        System.out.println(client.account().newOrder(parameters));
 
-        String result = client.createTrade().newOrder(parameters);
+        parameters.put("type", "TRAILING_STOP_MARKET");
 
-        System.out.println(result);
+        var direction = "BUY";
+        var price = 0.99;
+        if(request.getDirection().equals("BUY")) {
+            direction = "SELL";
+            price = 1.01;
+        }
+        parameters.put("side", direction);
+        parameters.put("callbackRate", properties.getTrailingDelta());
+        parameters.put("timeInForce", "GTC");
+        parameters.put("price", request.getPrice() * price);
+
+        System.out.println(client.account().newOrder(parameters));
     }
 }
