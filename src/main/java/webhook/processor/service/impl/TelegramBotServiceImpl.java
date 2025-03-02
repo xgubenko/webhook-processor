dc32126d9430cdb8a23dc579535e8044e29b8d43
xgubenko <xgubenko@gmail.com>
2025-03-02 12:00:08 +0300

    binance update

--- src/main/java/webhook/processor/service/impl/TelegramBotServiceImpl.java
+++ src/main/java/webhook/processor/service/impl/TelegramBotServiceImpl.java
@@ -75,7 +75,7 @@ public class TelegramBotServiceImpl extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasChannelPost() && update.getChannelPost().hasVideo()) {
+        if (update.hasChannelPost() && update.getChannelPost().hasVideoNote()) {
             // Проверяем, является ли видео кругом (например, по разрешению)
             // Это условие нужно уточнить в зависимости от ваших требований
 //            if (isCircleVideo(update.getMessage().getVideo())) {
